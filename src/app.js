5152c79b78e8bca0f553787f43134f1a47e99feb
Samuel Gauffin Holmberg <samuel.gh@gmail.com>
2016-03-21 20:59:40 +0100

    second commit

--- src/app.js
+++ src/app.js
@@ -28,9 +28,9 @@ if (!process.argv[2]) {
   }
 
   function sendSerialData(data) {
-    var trimmedData = data.substring(data.indexOf(':') + 1).trim();
-    console.log(trimmedData);
-    request('https://api.thingspeak.com/update?api_key=CQS552MLOQ7ZM0Q5&field2=' + trimmedData ,
+    console.log("data:" + data);
+    var jsonData = JSON.parse(data);
+    request('https://api.thingspeak.com/update?api_key=CQS552MLOQ7ZM0Q5&field1=' + data.volt1 +'&field2=' + data.temp ,
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log('Sent request, got response:' + body) // Show the HTML for the Google homepage.
