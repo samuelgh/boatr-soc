b9ed000facfab559590987e8597f76f9417d796f
Samuel Gauffin Holmberg <samuel.gh@gmail.com>
2016-03-22 07:28:51 +0100

    Removed unecessary comments

--- arduino-src/linkit-temp-volt/linkit-temp-volt.ino
+++ arduino-src/linkit-temp-volt/linkit-temp-volt.ino
@@ -2,20 +2,15 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
  
-// Data wire is plugged into pin 2 on the Arduino
 #define ONE_WIRE_BUS 12
 #define VOLT_1 10
 
-// Setup a oneWire instance to communicate with any OneWire devices 
-// (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
- 
-// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 unsigned long previousMillis = 0;
 unsigned long voltPreviousMillis = 0;
-const long interval = 2000;
+const long interval = 15000;
 const long voltMeasureInterval = 5000;
 
 
@@ -25,8 +20,6 @@ float r1 = 87500; //100000;
 float r2 = 9600; //10000;
 
 void setup()  {
-  // define pin modes for tx, rx:
-  // set the data rate for the SoftwareSerial port
   Serial.begin(115200);  // Datorn
   Serial1.begin(57600);  // Till MT7688
   sensors.begin();
@@ -66,7 +59,9 @@ void loop() {
     data["temp"] = measureTemp();
     data["volt1"] = measureVolt();
     data.printTo(Serial);
+    data.printTo(Serial1);
     Serial.println("");
+    Serial1.println("");
   }
   if (Serial.available()) {
     c = Serial.read();
