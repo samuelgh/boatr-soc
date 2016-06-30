a5627f9bce42b528b59d211d415ca5ca884a333e
Samuel Gauffin Holmberg <samuel.gh@gmail.com>
2016-06-30 21:09:40 +0200

    Latest linkit python script

--- data-sender.py
+++ data-sender.py
@@ -4,15 +4,18 @@ import urllib2
 
 s = None
 def setup():
-	global s
-	s = serial.Serial("/dev/ttyS0", 57600)
+        print('Start setup')
+        global s
+        s = serial.Serial('/dev/ttyS0', 57600)
 def loop():
-    data = json.loads(s.readline())
-    print(data)
-    url = 'https://api.thingspeak.com/update?api_key=CQS552MLOQ7ZM0Q5&field1='
-    + data['volt1'] +'&field2=' + data['temp'] +'&field3=10'
-
+        print('looping')
+        data = json.loads(s.readline())
+        print(data)
+        url = ('https://api.thingspeak.com/update?api_key=CQS552MLOQ7ZM0Q5&field1='
+        + str(data['volt1']) +'&field2=' + str(data['temp']) +'&field3=10')
+        print(urllib2.urlopen(url).read())
 if __name__ == '__main__':
-	setup()
-	while True:
-		loop()
+        print('Staring program')
+        setup()
+        while True:
+                loop()
